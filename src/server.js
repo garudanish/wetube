5739bb0a61209e189bb0818411b46e1cb1ecc053
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-07-13 20:49:56 +0900

    3.11 External Middlewares

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
